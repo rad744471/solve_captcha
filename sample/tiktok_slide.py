8cfed7a0d23139d32965a849772966d61c55af1b
rad744471 <162858129+rad744471@users.noreply.github.com>
2024-03-16 11:27:49 +0700

    Create tiktok_slide.py

---
+++ sample/tiktok_slide.py
@@ -0,0 +1,27 @@
+import time
+import requests
+
+# Tạo Job
+json_data = {
+    "api_token": "YOUR_API_KEY",
+    "data": {
+        "type_job": "tiktok_slide",
+        "body": "image as base64 encoded"
+    }
+}
+createJob = requests.post("http://betacaptcha.com/api/createJob", json=json_data)
+
+# Lấy kết quả trả về
+for _ in range(3):
+    json_data = {
+        "api_token": "YOUR_API_KEY",
+        "taskid": createJob.json()["taskid"]
+    }
+    getJobResult = requests.post("http://betacaptcha.com/api/getJobResult", json=json_data)
+    if getJobResult.json()["status"] != "running":
+        result = getJobResult.json()["result"]
+        break
+    else:
+        time.sleep(1)
+
+print(">> Kết quả:", result)
